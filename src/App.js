73ad8b9d415bac73aaba75c2c19ad2b47a9f1b77
kunrenyouAccount <1214kunrenyou@gmail.com>
2021-01-02 18:09:29 +0900

    useStateを使用した状態管理まで作成

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Fragment, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [text, setText] = useState('');
+
+  const changedText = (e) => {
+    setText(e.target.value);
+  }
+
+  const clickedButton = () => {
+    alert(text);
+  }
+
   return (
-    <h1>Hello! React!!</h1>
+    <Fragment>
+      <h1>Hello! React!!</h1>
+      <input type="text" value={text} onChange={changedText}/><br />
+      <button onClick={clickedButton}>Click Me!!!</button>
+    </Fragment>
   );
 }
 
