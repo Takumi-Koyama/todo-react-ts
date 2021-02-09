2f1291de4d2a3da34ffe508be4399f8fbefbff46
kunrenyouAccount <1214kunrenyou@gmail.com>
2021-02-09 13:44:29 +0900

    ESLint+Prettier導入

---
+++ typescript/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    ecmaFeatures: { jsx: true },
+    sourceType: 'module',
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+  env: {
+    es2021: true,
+    //DOMが使えるようになる
+    browser: true,
+    //javascriptのテストコードを書く時に使うjestに対しても有効
+    jest: true,
+    node: true,
+  },
+  plugins: [
+    'react-hooks',
+    'react',
+    '@typescript-eslint',
+    'eslint-plugin-prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'info', 'error'] }],
+    'prefer-arrow-callback': 'error',
+    'func-style': ['error', 'expression'],
+    'arrow-body-style': ['error', 'always'],
+    'react/react-in-jsx-scope': 0,
+    'react/display-name': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+};
