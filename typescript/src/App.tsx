64399b27d2c8e15fe44a9ffb046ba7026658f959
kunrenyouAccount <1214kunrenyou@gmail.com>
2021-02-11 17:32:03 +0900

    追加、更新処理まで作成

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,6 +1,7 @@
 import styles from './App.css';
 import React from 'react';
 import { TodoList } from 'src/views/TodoList/TodoList';
+import { TodoListProvider } from 'src/context/TodoListContext';
 import Axios from 'axios';
 
 Axios.defaults.baseURL = 'http://localhost:4000/api/';
@@ -8,7 +9,9 @@ Axios.defaults.baseURL = 'http://localhost:4000/api/';
 export const App: React.FC = () => {
   return (
     <div className={styles.area}>
-      <TodoList />
+      <TodoListProvider>
+        <TodoList />
+      </TodoListProvider>
     </div>
   );
 };
--- typescript/src/views/TodoList/TodoList.tsx
+++ typescript/src/views/TodoList/TodoList.tsx
@@ -1,28 +1,73 @@
 import styles from './TodoList.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Todo } from 'src/model/Todo';
 import { TodoItem } from 'src/components/TodoItem';
-import Axios from 'axios';
+import Axios, { AxiosResponse } from 'axios';
+import { TodoListContext } from 'src/context/TodoListContext';
 
 export const TodoList: React.FC = () => {
-  const [todoList, setTodoList] = useState<Todo[]>([]);
+  // const [title, setTitle] = useState('');
+  // const [description, setDescription] = useState('');
+
+  const [todo, setTodo] = useState<Todo>({
+    title: '',
+    description: '',
+  });
+  // const [todoList, setTodoList] = useState<Todo[]>([]);
+  const { todoList, setTodoList } = useContext(TodoListContext);
 
   //コンポーネントのマウント時に発動
   useEffect(() => {
-    const getTodoList = async () => {
+    // const getTodoList = async () => {
+    //   const response = await Axios.get<Todo[]>('todos');
+    //   setTodoList(response.data);
+    // };
+    // getTodoList();
+    (async () => {
       const response = await Axios.get<Todo[]>('todos');
       setTodoList(response.data);
-    };
-    getTodoList();
+    })();
   }, [setTodoList]);
+
+  const changedTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //左側の引数に対して、右側の値をマージする
+    const newTodo = Object.assign({}, todo);
+    newTodo.title = e.target.value;
+    setTodo(newTodo);
+  };
+
+  const changedDescription = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    //左側の引数に対して、右側の値をマージする
+    const newTodo = Object.assign({}, todo);
+    newTodo.description = e.target.value;
+    setTodo(newTodo);
+  };
+
+  const addClick = async () => {
+    const response = await Axios.post<Todo, AxiosResponse<string>>(
+      'todos',
+      todo
+    );
+    const newTodo = Object.assign({}, todo);
+    newTodo.id = parseInt(response.data);
+
+    const newTodoList = todoList.slice();
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  };
 
   return (
     <>
       <div>
-        <input className={styles.todoTitleInput} />
-        <textarea className={styles.todoDescriptionInput} />
+        <input className={styles.todoTitleInput} onChange={changedTitle} />
+        <textarea
+          className={styles.todoDescriptionInput}
+          onChange={changedDescription}
+        />
         <div>
-          <button className={styles.todoAddButton}>Click Me!!!</button>
+          <button className={styles.todoAddButton} onClick={addClick}>
+            Click Me!!!
+          </button>
         </div>
       </div>
       {todoList.map((todo) => {
